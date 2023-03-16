d62b693361529041757593fba78b55bec7654f4e
Duc Thu <tranducthu.99@gmail.com>
2023-03-17 00:35:27 +0700

    add: WIP MovieView SimplePiP

--- SimplePictureInPicture/app/src/main/java/dev/ducthu/simplepictureinpicture/widget/MovieView.kt
+++ SimplePictureInPicture/app/src/main/java/dev/ducthu/simplepictureinpicture/widget/MovieView.kt
@@ -5,6 +5,9 @@ import android.content.res.AssetFileDescriptor
 import android.content.res.TypedArray
 import android.graphics.Color
 import android.media.MediaPlayer
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.transition.TransitionManager
 import android.util.AttributeSet
 import android.util.Log
@@ -16,6 +19,7 @@ import android.widget.RelativeLayout
 import androidx.annotation.RawRes
 import dev.ducthu.simplepictureinpicture.R
 import java.io.IOException
+import java.lang.ref.WeakReference
 
 class MovieView : RelativeLayout {
     companion object {
@@ -25,6 +29,27 @@ class MovieView : RelativeLayout {
             fun onMovieStopped() {}
 
             fun onMovieMinimized() {}
+        }
+
+        class TimeoutHandler(view: MovieView) : Handler(Looper.getMainLooper()) {
+
+            companion object {
+                const val MESSAGE_HIDE_CONTROLS = 1
+            }
+
+            private val mMovieViewRef: WeakReference<MovieView>
+
+            override fun handleMessage(msg: Message) {
+                if (msg.what == MESSAGE_HIDE_CONTROLS) {
+                    mMovieViewRef.get()?.hideControls()
+                } else {
+                    super.handleMessage(msg)
+                }
+            }
+
+            init {
+                mMovieViewRef = WeakReference(view)
+            }
         }
 
         const val TAG = "MovieView"
@@ -46,7 +71,7 @@ class MovieView : RelativeLayout {
 
     private lateinit var mMinimize: ImageButton
 
-    private lateinit var mMediaPlayer: MediaPlayer
+    private var mMediaPlayer: MediaPlayer? = null
 
     @RawRes
     private var mVideoResourceId: Int = 0
@@ -55,16 +80,14 @@ class MovieView : RelativeLayout {
 
     private var mAdjustViewBounds: Boolean = false
 
-    private lateinit var mTimeOutHandler: TimeoutHandler
+    private var mTimeOutHandler: TimeoutHandler? = null
 
-    private lateinit var mMovieListener: MovieListener
+    private var mMovieListener: MovieListener? = null
 
     private var mSavedCurrentPosition: Int = 0
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
+        context, attrs, defStyleAttr
     ) {
         setBackgroundColor(Color.BLACK)
         inflate(context, R.layout.view_movie, this);
@@ -76,18 +99,89 @@ class MovieView : RelativeLayout {
         mMinimize = findViewById(R.id.minimize)
 
         val attributes: TypedArray = context.obtainStyledAttributes(
-            attrs,
-            R.styleable.MovieView,
-            defStyleAttr,
-            R.style.Widget_PictureInPicture_MovieView
+            attrs, R.styleable.MovieView, defStyleAttr, R.style.Widget_PictureInPicture_MovieView
         )
 
-        setVideoR
+
     }
 
     constructor(context: Context) : this(context, null, 0)
 
     constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val videoWidth = mMediaPlayer?.videoWidth ?: 0
+        val videoHeight = mMediaPlayer?.videoHeight ?: 0
+        if (videoWidth != 0 && videoHeight != 0) {
+            val aspectRatio: Float = videoHeight.toFloat() / videoWidth.toFloat()
+            val width = MeasureSpec.getSize(widthMeasureSpec)
+            val height = MeasureSpec.getSize(heightMeasureSpec)
+            val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+            val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+            if (mAdjustViewBounds) {
+                if (widthMode == MeasureSpec.EXACTLY && heightMode != MeasureSpec.EXACTLY) {
+                    super.onMeasure(
+                        widthMeasureSpec,
+                        MeasureSpec.makeMeasureSpec(
+                            (width * aspectRatio).toInt(),
+                            MeasureSpec.EXACTLY
+                        )
+                    )
+                } else if (widthMode != MeasureSpec.EXACTLY && heightMode == MeasureSpec.EXACTLY) {
+                    super.onMeasure(
+                        MeasureSpec.makeMeasureSpec(
+                            (height / aspectRatio).toInt(),
+                            heightMeasureSpec
+                        ),
+                        heightMeasureSpec
+                    )
+                } else {
+                    super.onMeasure(
+                        widthMeasureSpec,
+                        MeasureSpec.makeMeasureSpec(
+                            (width * aspectRatio).toInt(),
+                            MeasureSpec.EXACTLY
+                        )
+                    )
+                }
+            } else {
+                val viewRatio: Float = (height / width).toFloat()
+                if (aspectRatio > viewRatio) {
+                    val padding = ((width - height / aspectRatio) / 2).toInt()
+                    setPadding(padding, 0, padding, 0)
+                } else {
+                    val padding = ((height - width * aspectRatio) / 2).toInt()
+                    setPadding(0, padding, 0, padding)
+                }
+
+                super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+            }
+
+            return
+        }
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    override fun onDetachedFromWindow() {
+        mTimeOutHandler?.let {
+            it.removeMessages(TimeoutHandler.MESSAGE_HIDE_CONTROLS)
+            mTimeOutHandler = null
+        }
+        super.onDetachedFromWindow()
+    }
+
+    fun setMovieListener(movieListener: MovieListener?) {
+        mMovieListener = movieListener
+    }
+
+    fun setTitle(title: String) {
+        mTitle = title
+    }
+
+    fun getTitle(): String = mTitle
+
+    fun getVideoResourceId(): Int = mVideoResourceId
 
     fun setVideoResourceId(@RawRes id: Int) {
         if (id == mVideoResourceId) {
@@ -135,48 +229,56 @@ class MovieView : RelativeLayout {
     }
 
     fun fastForwards() {
-        mMediaPlayer.seekTo(mMediaPlayer.currentPosition + FAST_FORWARD_REWIND_INTERVAL)
+        mMediaPlayer?.let {
+            it.seekTo(
+                it.currentPosition + FAST_FORWARD_REWIND_INTERVAL
+            )
+        }
     }
 
     fun fastRewind() {
-        mMediaPlayer.seekTo(mMediaPlayer.currentPosition - FAST_FORWARD_REWIND_INTERVAL)
+        mMediaPlayer?.let { it.seekTo(it.currentPosition - FAST_FORWARD_REWIND_INTERVAL) }
     }
 
     fun setCurrentPosition(): Int {
-        return mMediaPlayer.currentPosition
+        return mMediaPlayer?.currentPosition ?: 0
     }
 
     fun isPlaying(): Boolean {
-        return mMediaPlayer.isPlaying
+        return mMediaPlayer?.isPlaying ?: false
     }
 
     fun play() {
-        mMediaPlayer.start()
-        adjustToggleState()
-        keepScreenOn = true
-        mMovieListener.onMovieStarted()
+        mMediaPlayer?.let {
+            it.start()
+            adjustToggleState()
+            keepScreenOn = true
+            mMovieListener.onMovieStarted()
+        }
     }
 
     fun pause() {
-        mMediaPlayer.pause()
-        adjustToggleState()
-        keepScreenOn = false
-        mMovieListener.onMovieStopped()
+        mMediaPlayer?.let {
+            it.pause()
+            adjustToggleState()
+            keepScreenOn = false
+            mMovieListener.onMovieStopped()
+        }
     }
 
     fun openVideo(surface: Surface) {
         if (mVideoResourceId == 0) return
         mMediaPlayer = MediaPlayer()
-        mMediaPlayer.setSurface(surface)
+        mMediaPlayer?.setSurface(surface)
         startVideo()
     }
 
     fun startVideo() {
-        mMediaPlayer.reset()
+        mMediaPlayer?.reset()
         try {
             val fd: AssetFileDescriptor = resources.openRawResourceFd(mVideoResourceId)
-            mMediaPlayer.setDataSource(fd)
-            mMediaPlayer.setOnPreparedListener { mediaPlayer ->
+            mMediaPlayer?.setDataSource(fd)
+            mMediaPlayer?.setOnPreparedListener { mediaPlayer ->
                 requestLayout()
                 if (mSavedCurrentPosition > 0) {
                     mediaPlayer.seekTo(mSavedCurrentPosition)
@@ -185,7 +287,7 @@ class MovieView : RelativeLayout {
                     play()
                 }
             }
-            mMediaPlayer.setOnCompletionListener { mediaPlayer ->
+            mMediaPlayer?.setOnCompletionListener { mediaPlayer ->
                 adjustToggleState()
                 keepScreenOn = false
                 mMovieListener.onMovieStopped()
@@ -196,11 +298,12 @@ class MovieView : RelativeLayout {
     }
 
     fun closeVideo() {
-        mMediaPlayer.release()
+        mMediaPlayer?.release()
+        mMediaPlayer = null
     }
 
     fun toggle() {
-        if (mMediaPlayer.isPlaying) {
+        if (mMediaPlayer?.isPlaying == true) {
             pause()
         } else {
             play()
@@ -216,9 +319,12 @@ class MovieView : RelativeLayout {
     }
 
     fun adjustToggleState() {
-        if(mMediaPlayer.isPlaying) {
+        if (mMediaPlayer != null && mMediaPlayer?.isPlaying == true) {
             mToggle.contentDescription = resources.getString(R.string.pause)
-            mToggle.setImageResource(R.drawable.)
+            mToggle.setImageResource(R.drawable.ic_pause_64dp)
+        } else {
+            mToggle.contentDescription = resources.getString(R.string.play)
+            mToggle.setImageResource(R.drawable.ic_play_arrow_64dp)
         }
     }
 }
