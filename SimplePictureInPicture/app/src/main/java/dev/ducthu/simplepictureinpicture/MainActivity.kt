fc253e561150efebdb88e7b83d1689c62787cfe3
Duc Thu <tranducthu.99@gmail.com>
2023-03-16 21:21:42 +0700

    add: setup SimplePictureInPicture

---
+++ SimplePictureInPicture/app/src/main/java/dev/ducthu/simplepictureinpicture/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.ducthu.simplepictureinpicture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
