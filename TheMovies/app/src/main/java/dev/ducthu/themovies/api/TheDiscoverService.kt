e6f8d8eb263737eb0206c180f626840b60865e11
Duc Thu <tranducthu.99@gmail.com>
2023-03-18 23:54:35 +0700

    add: complete services in api

---
+++ TheMovies/app/src/main/java/dev/ducthu/themovies/api/TheDiscoverService.kt
@@ -0,0 +1,13 @@
+package dev.ducthu.themovies.api
+
+import androidx.lifecycle.LiveData
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TheDiscoverService {
+    @GET("/3/discover/movie?language=en&sort_by=popularity.desc")
+    fun fetchDiscoverMovie(@Query("page") page: Int): LiveData<ApiResponse<DiscoverMovieResponse>>
+
+    @GET("/3/discover/tv?language=en&sort_by=popularity.desc")
+    fun fetchDiscoverTv(@Query("page") page: Int): LiveData<ApiResponse<DiscoverTvResponse>>
+}
---
+++ TheMovies/app/src/main/java/dev/ducthu/themovies/api/TvService.kt
@@ -0,0 +1,16 @@
+package dev.ducthu.themovies.api
+
+import androidx.lifecycle.LiveData
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface TvService {
+    @GET("/3/tv/{tv_id}/keywords")
+    fun fetchKeywords(@Path("tv_id") id: Int): LiveData<ApiResponse<KeywordListResponse>>
+
+    @GET("/3/tv/{tv_id}/videos")
+    fun fetchVideos(@Path("tv_id") id: Int): LiveData<ApiResponse<VideoListResponse>>
+
+    @GET("/3/tv/{tv_id}/reviews")
+    fun fetchReviews(@Path("tv_id") id: Int): LiveData<ApiResponse<ReviewListResponse>>
+}
