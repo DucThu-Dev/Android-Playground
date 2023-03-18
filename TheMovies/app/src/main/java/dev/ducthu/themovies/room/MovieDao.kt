cbb1a614e2fcae441714554837f7b7d7afbbf371
Duc Thu <tranducthu.99@gmail.com>
2023-03-19 00:58:36 +0700

    add: complete room

---
+++ TheMovies/app/src/main/java/dev/ducthu/themovies/room/MovieDao.kt
@@ -0,0 +1,19 @@
+package dev.ducthu.themovies.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface MovieDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertMovieList(movies: List<Movie>)
+
+    @Update
+    fun updateMovie(movie: Movie)
+
+    @Query("SELECT * FROM MOVIE WHERE id = :id_")
+    fun getMovie(id_: Int): Movie
+
+    @Query("SELECT * FROM MOVIE WHERE page = :page_")
+    fun getMovieList(page_: Int): LiveData<List<Movie>>
+}
